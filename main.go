7f777319c94b871933efd490d246d8a1896060c7
Glatharth <phlinho10@gmail.com>
2021-06-08 19:16:58 -0300

    fix bug

--- main.go
+++ main.go
@@ -11,23 +11,23 @@ import (
 )
 
 type Spells struct {
-	XMLName		xml.Name	`xml:"spells"`
-	Spells		[]Spell		`xml:"instant"`
+	XMLName xml.Name `xml:"spells"`
+	Spells  []Spell  `xml:"instant"`
 }
 
 type Spell struct {
-	XMLName		xml.Name 	`xml:"instant"`
-	Name		string		`xml:"name,attr"`
-	Words		string		`xml:"words,attr"`
-	Aggressive	string		`xml:"aggressive,attr"`
-	BlockWalls	string		`xml:"blockwalls,attr"`
-	NeedTarget	string		`xml:"needtarget,attr"`
-	NeedLearn	string		`xml:"needlearn,attr"`
-	Direction	string		`xml:"direction,attr"`
-	Exhaustion	string		`xml:"exhaustion,attr"`
-	SelfTarget	string		`xml:"selftarget,attr"`
-	Range		string		`xml:"range,attr"`
-	Script		string		`xml:"script,attr"`
+	XMLName    xml.Name `xml:"instant"`
+	Name       string   `xml:"name,attr"`
+	Words      string   `xml:"words,attr"`
+	Aggressive string   `xml:"aggressive,attr"`
+	BlockWalls string   `xml:"blockwalls,attr"`
+	NeedTarget string   `xml:"needtarget,attr"`
+	NeedLearn  string   `xml:"needlearn,attr"`
+	Direction  string   `xml:"direction,attr"`
+	Exhaustion string   `xml:"exhaustion,attr"`
+	SelfTarget string   `xml:"selftarget,attr"`
+	Range      string   `xml:"range,attr"`
+	Script     string   `xml:"script,attr"`
 }
 
 func openFile(path string) ([]byte, error) {
@@ -64,45 +64,45 @@ func main() {
 		}
 
 		output := bytes.Replace(input, []byte("function onCastSpell("), []byte("local spell = Spell(\"instant\")\nfunction spell.onCastSpell("), -1)
-		if err = ioutil.WriteFile("new/" + spell.Script, output, 0666); err != nil {
+		if err = ioutil.WriteFile("new/"+spell.Script, output, 0666); err != nil {
 			log.Fatal(err)
 		}
 
-		f, err := os.OpenFile("new/" + spell.Script, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		f, err := os.OpenFile("new/"+spell.Script, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 		if err != nil {
 			log.Fatal(err)
 		}
 		var attr []string
 
 		if spell.Name != "" {
-			attr = append(attr, "spell:name(\"" + spell.Name + "\")")
+			attr = append(attr, "spell:name(\""+spell.Name+"\")")
 		}
 		if spell.Words != "" {
-			attr = append(attr, "spell:words(\"" + spell.Words + "\")")
+			attr = append(attr, "spell:words(\""+spell.Words+"\")")
 		}
-		if spell.Aggressive != "" {
+		if spell.Aggressive == "1" {
 			attr = append(attr, "spell:isAggressive(true)")
 		}
-		if spell.BlockWalls != "" {
+		if spell.BlockWalls == "1" {
 			attr = append(attr, "spell:blockWalls(true)")
 		}
-		if spell.NeedTarget != "" {
+		if spell.NeedTarget == "1" {
 			attr = append(attr, "spell:needTarget(true)")
 		}
-		if spell.NeedLearn != "" {
+		if spell.NeedLearn == "1" {
 			attr = append(attr, "spell:needLearn(true)")
 		}
-		if spell.Direction != "" {
+		if spell.Direction == "1" {
 			attr = append(attr, "spell:needDirection(true)")
 		}
 		if spell.Exhaustion != "" {
-			attr = append(attr, "spell:exhaustion(\"" + spell.Exhaustion + "\")")
+			attr = append(attr, "spell:exhaustion(\""+spell.Exhaustion+"\")")
 		}
-		if spell.SelfTarget != "" {
+		if spell.SelfTarget == "1" {
 			attr = append(attr, "spell:selfTarget(true)")
 		}
 		if spell.Range != "" {
-			attr = append(attr, "spell:selfTarget(\"" + spell.Range + "\")")
+			attr = append(attr, "spell:selfTarget(\""+spell.Range+"\")")
 		}
 
 		for u := 0; u < len(attr); u++ {
